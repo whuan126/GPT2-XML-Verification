b07211c0f4007c5b7176e9d2c00035a77b3f00ea
jto7646 <jto7646@gmail.com>
2023-11-24 19:55:56 -0800

    Create xmlVerify.c
    Started work on small verification c program, then holiday stuff happened. This is what I've got!

---
+++ xmlVerify.c
@@ -0,0 +1,21 @@
+
+
+
+#include <stdio.h>
+// libxml2 parsing libraries
+#include <libxml/parser.h>
+#include <libxml/tree.h>
+// Library used to read in from directory
+#include <dirent.h>
+
+static void verify(const char *filename) {
+
+    // Some Code Here!
+}
+
+int main(int argc, char **argv) {
+    
+    // Some Code Here!
+
+    return 0;
+}
